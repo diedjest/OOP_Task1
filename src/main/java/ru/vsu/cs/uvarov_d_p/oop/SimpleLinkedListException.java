5d188a0ebfc3520d8cb4040e305b17ec8193e62d
Dmitriy Uvarov <103217621+diedjest@users.noreply.github.com>
2025-10-10 10:29:27 +0300

    Update SimpleLinkedListException.java

--- src/main/java/ru/vsu/cs/uvarov_d_p/oop/SimpleLinkedListException.java
+++ src/main/java/ru/vsu/cs/uvarov_d_p/oop/SimpleLinkedListException.java
@@ -1,6 +1,6 @@
 package ru.vsu.cs.uvarov_d_p.oop;
 
-class SimpleLinkedListException extends Exception {
+public class SimpleLinkedListException extends Exception {
     public SimpleLinkedListException(String error) {
         super(error);
     }
