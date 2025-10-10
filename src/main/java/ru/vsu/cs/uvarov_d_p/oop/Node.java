c371028bc9a3a6d5edac0e49d711a0fb08f8c602
Dmitriy Uvarov <103217621+diedjest@users.noreply.github.com>
2025-10-10 10:29:05 +0300

    Update Node.java

--- src/main/java/ru/vsu/cs/uvarov_d_p/oop/Node.java
+++ src/main/java/ru/vsu/cs/uvarov_d_p/oop/Node.java
@@ -1,9 +1,9 @@
 package ru.vsu.cs.uvarov_d_p.oop;
 
 public class Node<T> {
-    T value;
-    Node<T> next;
-    Node<T> prev;
+    public T value;
+    public Node<T> next;
+    public Node<T> prev;
 
     public Node(T value, Node<T> next, Node<T> prev) {
         this.value = value;
