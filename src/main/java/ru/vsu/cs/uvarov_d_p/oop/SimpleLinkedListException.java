10a07703ccbc4b23f2dc0ca6ee7f3a31e792fafa
Dmitriy Uvarov <103217621+diedjest@users.noreply.github.com>
2025-10-15 20:33:31 +0300

    Delete src/main/java/ru/vsu/cs/uvarov_d_p/oop directory

--- src/main/java/ru/vsu/cs/uvarov_d_p/oop/SimpleLinkedListException.java
+++
@@ -1,7 +0,0 @@
-package ru.vsu.cs.uvarov_d_p.oop;
-
-public class SimpleLinkedListException extends Exception {
-    public SimpleLinkedListException(String error) {
-        super(error);
-    }
-}
