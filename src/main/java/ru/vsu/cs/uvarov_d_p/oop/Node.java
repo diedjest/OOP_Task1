f8e2c224b671057df3b0dd04bbf679608b75f893
Dmitriy Uvarov <103217621+diedjest@users.noreply.github.com>
2025-10-15 20:13:42 +0300

    Add files via upload

---
+++ src/main/java/ru/vsu/cs/uvarov_d_p/oop/Node.java
@@ -0,0 +1,17 @@
+package ru.vsu.cs.uvarov_d_p.oop;
+
+public class Node<T> {
+    public T value;
+    public Node<T> next;
+    public Node<T> prev;
+
+    public Node(T value, Node<T> next, Node<T> prev) {
+        this.value = value;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    public Node(T value) {
+        this(value, null, null);;
+    }
+}
