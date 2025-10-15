b50f7b0559994ff73f74a9b40408c32aaac1dd94
Dmitriy Uvarov <103217621+diedjest@users.noreply.github.com>
2025-10-15 22:10:00 +0300

    refactor directories and packages

---
+++ src/main/java/ru/vsu/cs/uvarov_d_p/oop/linked_list/SimpleLinkedListException.java
@@ -0,0 +1,7 @@
+package ru.vsu.cs.uvarov_d_p.oop.linked_list;
+
+public class SimpleLinkedListException extends Exception {
+    public SimpleLinkedListException(String error) {
+        super(error);
+    }
+}
